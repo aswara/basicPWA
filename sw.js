0097359ba3bda5101a7d29df029f381da4a7b253
Aswara <a.aswara@gmail.com>
2018-12-15 17:18:30 +0700

    notification

--- sw.js
+++ sw.js
@@ -12,28 +12,14 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
+        console.log(cache)
         console.log('in install service worker Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
 });
 
-// self.addEventListener('fetch', function(event) {
-//   event.respondWith(
-//     caches.match(event.request)
-//       .then(function(response) {
-//         // Cache hit - return response
-//         if (response) {
-//           return response;
-//         }
-//         return fetch(event.request);
-//       }
-//     )
-//   );
-// });
-
 self.addEventListener('fetch', function(event) {
-
   event.respondWith(
     caches.match(event.request)
       .then(function(response) {
@@ -49,11 +35,10 @@ self.addEventListener('fetch', function(event) {
 
 
 
-
 self.addEventListener('activate', function(event) {
-
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
+      console.log(cacheNames)
       return Promise.all(
         cacheNames.filter(function(cacheNames){
           return cacheNames != CACHE_NAME
@@ -61,6 +46,46 @@ self.addEventListener('activate', function(event) {
           return caches.delete(cacheName)
         })
       );
+    })
+  );
+});
+
+//push notification
+self.addEventListener('push', function(event) {
+  console.log('[Service Worker] Push Received.');
+  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+  const title = 'Push Codelab';
+  const options = {
+    body: 'Yay it works.',
+    icon: 'images/icon.png',
+    badge: 'images/badge.png'
+  };
+
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('[Service Worker] Notification click Received.');
+
+  event.notification.close();
+
+  event.waitUntil(
+    clients.openWindow('https://developers.google.com/web/')
+  );
+});
+
+self.addEventListener('pushsubscriptionchange', function(event) {
+  console.log('[Service Worker]: \'pushsubscriptionchange\' event fired.');
+  const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
+  event.waitUntil(
+    self.registration.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: applicationServerKey
+    })
+    .then(function(newSubscription) {
+      // TODO: Send to application server
+      console.log('[Service Worker] New subscription: ', newSubscription);
     })
   );
 });
